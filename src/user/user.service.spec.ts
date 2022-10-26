5032947ee3f07572b7dbae82452f0ff480b08bf6
JNotelddim <JNotelddim@users.noreply.github.com>
2022-10-26 15:17:53 -0700

    Revert "Use TestingModule and replace absolute imports w/ relative ones"
    This reverts commit fc44efa1b98be7e9804c089e96ae22aac1943e92.

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,37 +1,29 @@
-import { Test } from '@nestjs/testing';
+import { DatabaseService } from 'src/database/database.service';
+import { mockUsers } from 'src/test/fixtures/user.fixtures';
 import { UserService } from './user.service';
-import { DatabaseService } from '../database/database.service';
-import { mockUsers } from '../test/fixtures/user.fixtures';
 
 /**
  * UNIT TESTS
  * on individual UserService methods
  */
 describe('UserService Unit Tests', () => {
+  let databseService: DatabaseService;
   let userService: UserService;
 
-  beforeEach(async () => {
-    const moduleRef = await Test.createTestingModule({
-      providers: [UserService, DatabaseService],
-    })
-      .useMocker((token) => {
-        if (token === DatabaseService) {
-          return {
-            user: { findUnique: jest.fn().mockResolvedValue(mockUsers[0]) },
-          };
-        }
-      })
-      .compile();
-
-    userService = moduleRef.get(UserService);
+  beforeEach(() => {
+    databseService = new DatabaseService();
+    userService = new UserService(databseService);
   });
 
-  it('`user` fn gets an individual user by id', async () => {
+  it('`user` fn gets an individual user by id', () => {
     // Arrange
-    const expected = mockUsers[0];
+    const expected = mockUsers;
+    jest
+      .spyOn(databseService.user, 'findUnique')
+      .mockImplementation(() => mockUsers[0] as any);
 
     // Act
-    const result = await userService.user({ id: mockUsers[0].id });
+    const result = userService.user({ id: mockUsers[0].id });
 
     // Assert
     expect(result).toBe(expected);
