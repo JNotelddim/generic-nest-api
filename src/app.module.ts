2c45e11c36ba108949b71a4c59e2e0d9271ba012
JNotelddim <JNotelddim@users.noreply.github.com>
2022-10-27 14:56:09 -0700

    Add GraphQL using Apollo driver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
